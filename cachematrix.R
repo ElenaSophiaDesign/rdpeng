71bf42580fa45de61072cd11ce3ed9a72c2627af
ElenaSophiaDesign <elenasophiadesign.gmail>
2015-07-25 14:57:49 -0500

    Coursera R Programming 2nd Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -3,60 +3,79 @@
 ##Explain the code 
 
 ## This funtion creates a special matrix object that can cache its inverse
-+makeCacheMatrix <- function( m = matrix() ) {
-+
-+	## The process initialize the inverse property
-+    i <- NULL
-+
-+    ## Process to set the matrix
-+    set <- function( matrix ) {
-+            m <<- matrix
-+            i <<- NULL
-+    }
-+
-+    ## Process the get the matrix
-+    get <- function() {
-+    	## Return the matrix
-+    	m
-+    }
-+
-+    ## Process to set the inverse of the matrix
-+    setInverse <- function(inverse) {
-+        i <<- inverse
-+    }
-+
-+    ## Process to get the inverse of the matrix
-+    getInverse <- function() {
-+        ## Return the inverse property
-+        i
-+    }
-+
-+    ## Return a list of the processes facts
-+    list(set = set, get = get,
-+         setInverse = setInverse,
-+         getInverse = getInverse)
-+}
-+
-+
-+## Compute the inverse of the matrix above 
-+cacheSolve <- function(x, ...) {
-+
-+    ## Return a matrix that is the inverse of 'x'
-+    m <- x$getInverse()
-+
-+    ## Return the inverse if its already set
-+    if( !is.null(m) ) {
-+            message("getting cached data")
-+            return(m)
-+    }
-+
-+    ## Get the matrix from the data 
-+    data <- x$get()
-+
-+    ## Use matrix multiplication to get the inverse 
-+    m <- solve(data) %*% data
-+
-+    ## Set the inverse
-+    x$setInverse(m)
-+
-+    ## Print the matrix
+makeCacheMatrix <- function( m = matrix() ) {
+
+	## The process initialize the inverse property
+	## and the identity matrix
+    i <- NULL
+	identityM <- NULL
+	
+	## Build identity matrix of the same dimensions as your input matrix 
+	d <- nrow(m)
+	identityM <- diag(d)
+	
+	## Process to set the matrix
+    set <- function( matrix ) {
+            m <<- matrix
+            i <<- NULL
+    }
+
+    ## Process the get the matrix
+    get <- function() {
+    	## Return the matrix
+    	m
+    }
+
+    ## Process to set the inverse of the matrix
+    setInverse <- function(inverse) {
+        i <<- inverse
+    }
+
+    ## Process to get the inverse of the matrix
+    getInverse <- function() {
+        ## Return the inverse property
+        i
+    }
+
+    ## Return a list of the processes facts
+	## including identity matrix of the same dimensions as the input matrix
+	
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse,
+		 identityM = identityM )
+}
+
+
+## Compute the inverse of the matrix above 
+cacheSolve <- function(x, ...) {	
+
+    ## Return a matrix that is the inverse of 'x'
+    i <- x$getInverse()
+
+    ## Return the inverse if its already set
+    if( !is.null(i) ) {
+            message("getting cached data")
+            return(i)
+    }
+
+    ## Get the matrix from the data 
+    data <- x$get()
+	
+	## get the identity matrix corresponding to the data
+	identityM <- x$identityM 
+	
+    ## Use matrix multiplication to get the inverse 
+
+    i <- solve(data) %*% identityM
+
+    ## Set the inverse
+    x$setInverse(i)
+
+    ## Print the matrix
+	print (i)
+	
+	}
+
+	
+	
